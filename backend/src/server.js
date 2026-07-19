bc7c658a93ad30f643cbe3af6fc90bf1c819119b
Falah KK <kkfalah@gmail.com>
2026-07-19 23:01:23 +0530

    db config and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
+import { clerkMiddleware } from '@clerk/express'
+
 import { ENV } from './config/env.js'
+import { connectDB } from './config/db.js';
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware());
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({message: "Success"});
@@ -20,4 +25,7 @@ if(ENV.NODE_ENV === "development") {
     });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is up and running."));
+app.listen(ENV.PORT, () => {
+    console.log("Server is up and running.");
+    connectDB();
+});
